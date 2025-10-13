b34a4850c023646fcf938ff02df3f51e6f9a7b47
Rim Elrhezzal <elrhezzalrim@gmail.com>
2025-10-13 23:15:25 +0100

    initial commit- chatbot project

---
+++ db.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || 'chatboty2',
+  port: process.env.DB_PORT || 14105,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("❌ Database connection failed:", err.message);
+  } else {
+    console.log("✅ Connected to MySQL database!");
+    connection.release();
+  }
+});
+
+module.exports = pool.promise();
