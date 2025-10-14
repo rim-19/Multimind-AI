7128ad77fe92fa37d32d20bb7d2a10d38e7101b9
Rim Elrhezzal <elrhezzalrim@gmail.com>
2025-10-14 19:58:23 +0100

    update db.js

--- db.js
+++ db.js
@@ -1,24 +1,17 @@
+$
 const mysql = require('mysql2');
-require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
-});
-
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error("❌ Database connection failed:", err.message);
-  } else {
-    console.log("✅ Connected to MySQL database!");
-    connection.release();
-  }
+  queueLimit: 0
 });
 
 module.exports = pool.promise();
+
+
