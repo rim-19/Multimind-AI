172488dc1cf8990a6ee429a70e10f9273388a536
Rim Elrhezzal <elrhezzalrim@gmail.com>
2025-10-14 13:56:26 +0100

    update db.js to use environment variables for deployment

--- db.js
+++ db.js
@@ -2,14 +2,14 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'chatboty2',
-  port: process.env.DB_PORT || 14105,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
 
 pool.getConnection((err, connection) => {
